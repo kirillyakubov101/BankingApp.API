09af9d88bf7ced8546ffb466f53e9d4900765b67
kirill.yakubov <kirillyakubov101@gmail.com>
2024-12-05 22:52:11 -0500

    First init and adding models

---
+++ Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace Banking.API.Models
+{
+    public class Account
+    {
+        public int Id { get; set; } // Unique identifier for the account
+        public string Name { get; set; } // Name of the account holder
+        public decimal Balance { get; set; } // Current balance of the account
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Account creation timestamp
+    }
+}
+    
